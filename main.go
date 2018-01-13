82b950e15ba78ffbfb7e64be63839d1524d57aae
Freddie <freddiestoddart000@gmail.com>
2018-01-13 14:45:50 +1300

    Implemented the tiedot database

--- main.go
+++ main.go
@@ -6,24 +6,63 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+
+	"github.com/HouzuoGuo/tiedot/db"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
+	fmt.Println("Welcome to the homepage")
 }
 
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// DB stuff here
+	fmt.Println("About to request collection")
+	col := myDB.Use("names")
+	fmt.Println("collection requested")
+	col.Insert(map[string]interface{}{
+		"name": ps.ByName("name"),
+	})
+	fmt.Println("Data added")
 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
 func Names(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	var names []byte
-	// DB stuff here
+	var names string
+	col := myDB.Use("names")
+	col.ForEachDoc(func(id int, docContent []byte) (willMoveOn bool) {
+		names = names + fmt.Sprintln("Document", id, "is", string(docContent))
+		return true  // move on to the next document OR
+		return false // do not move on to the next document
+	})
 	fmt.Fprintf(w, "names =, %s!\n", string(names))
 }
 
+var myDB *db.DB
+
 func main() {
+	// (Create if not exist) open a database
+	var err error
+	myDB, err = db.OpenDB("tmp/MyDatabase")
+	if err != nil {
+		panic(err)
+	}
+	defer myDB.Close()
+
+	myDB.Create("names")
+
+	col := myDB.Use("names")
+	fmt.Println("collection requested")
+	col.Insert(map[string]interface{}{
+		"name": "pwed",
+	})
+	fmt.Println("Data added")
+
+	col = myDB.Use("names")
+	col.ForEachDoc(func(id int, docContent []byte) (willMoveOn bool) {
+		fmt.Println("Document", id, "is", string(docContent))
+		return true  // move on to the next document OR
+		return false // do not move on to the next document
+	})
 
 	router := httprouter.New()
 	router.GET("/", Index)
