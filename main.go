f46fdea15a036a09e6ffd3a641e0a32b5bf4f758
Freddie <freddiestoddart000@gmail.com>
2018-01-13 13:39:01 +1300

    removed bolt db

--- main.go
+++ main.go
@@ -4,11 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/julienschmidt/httprouter"
-
-	bolt "github.com/coreos/bbolt"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -16,46 +13,17 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("names"))
-		err := b.Put([]byte("name"), []byte(ps.ByName("name")))
-		return err
-	})
+	// DB stuff here
 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
 func Names(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	var names []byte
-	db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("names"))
-		if b == nil {
-			return fmt.Errorf("Bucket %q not found!", "names")
-		}
-		v := b.Get([]byte("name"))
-		fmt.Print(string(v))
-		names = v
-		return nil
-	})
+	// DB stuff here
 	fmt.Fprintf(w, "names =, %s!\n", string(names))
 }
 
-// Open the my.db data file in your current directory.
-// It will be created if it doesn't exist.
-// Process will timeout after 1 second if file can't be opened.
-var db, err = bolt.Open("my.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
-
 func main() {
-	db.Update(func(tx *bolt.Tx) error {
-
-		tx.CreateBucketIfNotExists([]byte("names"))
-
-		return nil
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
 
 	router := httprouter.New()
 	router.GET("/", Index)
